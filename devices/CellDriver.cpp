7feb803ee64e9beddc7b6334f5ddecb23e96ab84
cheongchan <cheong.chan@yale.edu>
2013-03-25 01:12:55 -0300

    Update CellDriver.cpp

--- devices/CellDriver.cpp
+++ devices/CellDriver.cpp
@@ -1,81 +1,55 @@
-#include "CellDriver.h"     
-#include "Uart.h"    
+#include "CellDriver.h"
+#include <string.h>
 
 CellDriver::CellDriver(Uart* uart)
 {
-    
+    this->uart = uart;
 }
 
-    //Send AT+CPAS command Phone activity status
-    //return 0 if ready 1 if AT commands not available for other check documentation
-    int readyStatus() const;
-    {
+bool CellDriver::readyToSendTextMessage() const
+{
+    writeString("AT+CPAS\r\0");
+        
+    //insert some code to read the response
+    //Maybe do if statement depending on the response
+    
+    //return number of status
+}
 
-        writeString("AT+CPAS\r\0");
-        
-        //insert some code to read the response
-        //Maybe do if statement depending on the response
-        
-        //return number of status
+void CellDriver::queueTextMessage(const char* recipientPhoneNumber, const char* textMessage)
+{
+    std::stringstream totalMessage;
+    totalMessage << "AT+CMGS=\"" << recipientPhoneNumber << "\"\r\x1A";
+    totalMessage << textMessage << "\x1A";
+    
+    (*uart) << totalMessage.str();
+}
+
+bool CellDriver::update()
+{
      
-    }
+}
 
-    // True if this driver is ready to send another text message.
-    // This would be false if last queued message has not been sent yet.
-    bool readyToSendTextMessage() const;
-
-
-
-    
-    // Starts the process of sending a text message to the cellular module.
-    void queueTextMessage(const char* recipientPhoneNumber, const char* textMessage);
-    {  
-	//Send Message Function
-	//put command and cell phone together
-	//Need string.h probably
-	char msg_cmd [30];
-	//cmd for sending short sms
-	strcpy (msg_cmd, "AT+CMGS=\"");
-	//add phone number 1#######
-	strcat (msg_cmd, recipientPhoneNumber);
-	//add the ending part
-	strcat (msg_cmd, "\"\r\x1A\0");
-
-	//Format sms message
-	strcat (textMessage, "\x1A\0")
-
-	//Send message
-	writeString(msg_cmd);
-	writeString(texMessage);
-
-    } 
-
-    // Does incremental work on sending or receiving text messages.
-    // This function should be called periodically.
-    // Returns true if a new text message has just been made avaialable.
-    //Maybe just read all messages and deletes them read or unread at the end
-    bool update();    
-    {
-
-    }     
-    
-    // Copies up to (bufferSize - 1) bytes of the last received text message to destination,
-    // guaranteeing that the destination string is null terminated.
-    void getTextMessage(char* destination, size_t bufferSize) const;
-    {
-	//Read all messages if returns "ok" then no messages
-	writeString("AT+CMGL=\"ALL\"\r\0");
-	
+void CellDriver::getTextMessage(char* destination, size_t bufferSize) const
+{
+    //Read all messages if returns "ok" then no messages
+    (*uart) << "AT+CMGL=\"ALL\"\r";
     
     //
     //Reads the UART of messages here!!!!
     //
     
-    
-    
-	//Clean out all read messages
-	writeString("AT+CMGD=1,1\r\0");
-	  	 	
-    }
-    
+    //Clean out all read messages
+    (*uart) << "AT+CMGD=1,1\r";
+    //uart->writeString("AT+CMGD=1,1\r");
+}
 
+int32_t CellDriver::getCID() const
+{
+    
+}
+
+int32_t CellDriver::getMCC() const
+{
+    
+}
