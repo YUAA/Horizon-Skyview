58f72c2faa0771a029596f98a4e664ecb37fd728
james <jamesbatchelder@optonline.net>
2013-03-24 20:09:02 -0400

    Uploaded the GPS.h and GPS.cpp files
    These should work

--- devices/GPSDecoder.h
+++ devices/GPSDecoder.h
@@ -1,53 +1,85 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nmeaparse.h"
+
 #ifndef GPS_DECODER
 #define GPS_DECODER
 
-// Decodes bytes obtained from the GPS so that relevant details may be accessed.
 class GPSDecoder
 {
-    public:
+	public:
 
-    /*
-    Make use of int32_t, int16_t, int8_t (32-bits, 16-bits, or 8-bits) 
-    instead of int, short, or char. 
-    This will ensure that the length of the integer is always the same on different platforms.
-    */
+	/*
+	real in variable name indicates it is not fixed point
+	Make use of int32_t, int16_t, int8_t (32-bits, 16-bits, or 8-bits) 
+	instead of int, short, or char. 
+	This will ensure that the length of the integer is always the same on different platforms.
+	*/
 
-    // Value returned has the decimal point fixed at the 1000s place.
-    // Note that it is not in degrees.minutes form, but the numbers
-    // following the decimal point are a fraction of a degree.
-    int32_t getLatitude() const;
+	GPSDecoder();
 
-    // Value returned has the decimal point fixed at the 1000s place.
-    // Note that it is not in degrees.minutes form, but the numbers
-    // following the decimal point are a fraction of a degree.
-    int32_t getLongitude() const;
+	// Value returned has the decimal point fixed at the 1000s place.
+	// Note that it is not in degrees.minutes form, but the numbers
+	// following the decimal point are a fraction of a degree.
+	int32_t getLatitude() const;
 
-    // In millimeters. (Alternatively fixed-point meters, with the decimal at the 1000s place).
-    int32_t getAltitude() const;
+	// Value returned has the decimal point fixed at the 1000s place.
+	// Note that it is not in degrees.minutes form, but the numbers
+	// following the decimal point are a fraction of a degree.
+	int32_t getLongitude() const;
 
-    int8_t getSatelliteCount() const;
+	// In millimeters. (Alternatively fixed-point meters, with the decimal at the 1000s place).
+	int32_t getAltitude() const;
 
-    // In degrees. From true-north.
-    int32_t getTrueHeading() const;
+	int8_t getSatelliteCount() const;
 
-    // In degrees. From magnetic-north.
-    int32_t getMagneticHeading() const;
+	// In degrees. From true-north.
+	int32_t getTrueHeading() const;
 
-    // In millimeters/second.
-    int32_t getSpeed() const;
+	// In degrees. From magnetic-north.
+	int32_t getMagneticHeading() const;
 
-    // Horizontal Degrees of Precision
-    int32_t getHDOP() const;
-    
-    // Passes the GPSDecoder an additional byte from the the GPS's output stream
+	// In millimeters/second.
+	int32_t getSpeed() const;
+
+	// Horizontal Degrees of Precision
+	int32_t getHDOP() const;
+
+	// Passes the GPSDecoder an additional byte from the the GPS's output stream
     // to decode.
     // Returns true if the GPSDecoder has updated its parameters.
 	bool decodeByte(int8_t newByte);
 
-    private:
+	private:
+		NmeaData nmea;
 
-    // Your code here
+		char latitudeString[10];
+		char NSlatitudeString[1];
+		char longitudeString[10];
+		char EWlongitudeString[1];
+		char altitudeString[10];
+		char numSatellitesString[10];
+		char hdopString[10];
+
+		char speedString[10];
+		char trueheadingString[10];
+		char magneticheadingString[10];
+
+		int32_t latitude;
+		int32_t longitude;
+		int32_t altitute;
+		int32_t realNumSatellites;
+		int32_t realHdop;
+		int32_t speed;  
+		int32_t trueheading;
+		int32_t magneticheading;
+
 
 };
 
-#endif
+#endif //GPSDecoder
+/* assume NEMAinterperate gives 
+static char* datums[] = {gpsD.utc, latitude, latitudeDirection, longitude, longitudeDirection,
+                                 gpsD.satellites, gpsD.hdop, gpsD.altitude};
+								 */
